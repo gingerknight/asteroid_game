65ed6b74169a884762e15dd82c3ceb3214fa5663
gingerknight <ryanrichmond6@gmail.com>
2025-03-02 19:10:55 -0700

    ship movement and grouping

--- main.py
+++ main.py
@@ -13,7 +13,12 @@ def main():
     time_object = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 
     while True:
         for event in pygame.event.get():
@@ -21,7 +26,10 @@ def main():
                 return
 
         screen.fill("black")
-        player.draw(screen)
+        updatable.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         dt = time_object.tick(60) / 1000
     
